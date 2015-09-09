bf6df94ec73c029b159ebaf8f1814d614052578b
fcontreras2 <freddy.contreras3@gmail.com>
2015-09-08 21:42:20 -0430

    % Prueba de editar un texto

--- src/DSFacyt/InfrastructureBundle/Resources/public/assets/DSFacyt/angular/Text/textController.js
+++ src/DSFacyt/InfrastructureBundle/Resources/public/assets/DSFacyt/angular/Text/textController.js
@@ -18,11 +18,40 @@ text.controller('TextController', ['$scope','$filter', 'textService', '$modal', 
             alertEmptyData.$promise.then(function() {alertEmptyData.show();});
         }
 
+        //Declaración del modal de editar el texto
         var modalNewText = $modal({scope: $scope, template: 'modal-editText.tpl', show: false});
-        
+
+        //Variable que contendrá el ultimo texto que se dio click a editar
+        var lastModalText = {};
+
+        // La función muestra el modal de un editar un texto
         $scope.editText = function(indexPreview) {
+
+            lastModalText = {};
+            lastModalText.text_id = $scope.data[indexPreview].text_id;
+            lastModalText.start_date = $scope.data[indexPreview].start_date;
+            lastModalText.end_date = $scope.data[indexPreview].end_date;
+            lastModalText.title = $scope.data[indexPreview].title;
+            lastModalText.publish_time = $scope.data[indexPreview].publish_time;
+            lastModalText.info = $scope.data[indexPreview].info;
+            lastModalText.channels = $scope.data[indexPreview].channels;
+            lastModalText.status = $scope.data[indexPreview].status;
+
             $scope.indexPreview = indexPreview;
             modalNewText.$promise.then(modalNewText.show);
+        };
+
+        $scope.sendEditText = function(indexPreview) {
+
+            if ($scope.data[indexPreview] == lastModalText)
+                console.log('variable1');
+            else
+                console.log('variable2', lastModalText);
+
+            console.log('variable1', $scope.data[indexPreview]);
+            console.log('variable2', lastModalText);
+            //var url = Routing.generate('save_temp_room');
+
         };
 
     }]);
--- src/DSFacyt/InfrastructureBundle/Resources/public/assets/DSFacyt/angular/Text/textModule.js
+++ src/DSFacyt/InfrastructureBundle/Resources/public/assets/DSFacyt/angular/Text/textModule.js
@@ -1,7 +1,32 @@
 var text = angular.module("TextModule", ['angular-repeat-n', 'mgcrea.ngStrap'])
 
+    .config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    })
 
-.config(function($interpolateProvider){
-    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-});
+    .config(['$httpProvider',function ($httpProvider) {
+        $httpProvider.defaults.withCredentials = '';
+        $httpProvider.defaults.useXDomain = false;
+        $httpProvider.defaults.headers.common['X-Requested-With']= 'XMLHttpRequest';
+        var httpStatusCodeInterceptorFactory = function ($q) {
+            function onSuccess(response){
+                if("success_condition"){
+                    return response.data;
+                }
+                else{
+                    //Show your global error dialog $q.reject(response.data);//Very important to reject the error } };
+                    function onError(response){
+                        //Show your global error dialog
+                        $q.reject(response);
+                        //Very important to reject the error
+                    };
+                    return function (promise) {
+                        return promise.then(onSuccess,onError);
+                    };
+                };
+                //Activate your interceptor
+                $httpProvider.responseInterceptors.push(httpStatusCodeInterceptorFactory);
+            }
+        }
+    }]);
 

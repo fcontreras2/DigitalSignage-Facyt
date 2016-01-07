4a1627164f5dcec90e27aa5680198d055c645df6
fcontreras2 <freddy.contreras3@gmail.com>
2016-01-07 19:07:41 -0430

    + Agregando archivos de producción

---
+++ app/DoctrineMigrations/Version20160107182736.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160107182736 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE text ALTER publish_time TYPE VARCHAR(255)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE text ALTER publish_time TYPE TIME(0) WITHOUT TIME ZONE');
+    }
+}
---
+++ src/DSFacyt/InfrastructureBundle/Controller/Admin/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DSFacyt\InfrastructureBundle\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class DefaultController
+ *
+ * El siguiente controlador Se encarga de procesar y recibir las solicitudes referente
+ * a las vista iniciales del administrador
+ *
+ * @author Freddy Contreras <freddycontreras3@gmail.com>
+ * @author Currently Working: Freddy Contreras <freddycontreras3@gmail.com>
+ * @version 05/01/2016
+ * @package DSFacyt\InfrastructureBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * La siguiente función rendifica la vista del homepage del panel de administrador
+     *
+     * @author Freddy Contreras <freddycontreras3@gmail.com>
+     * @author Currently Working; Freddy Contreras <freddycontreras3@gmail.com>
+     * @version 05/01/2016
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {   
+        return new Response("Hola");
+        //return $this->render('DSFacytInfrastructureBundle:Default:index.html.twig');
+    }
+}

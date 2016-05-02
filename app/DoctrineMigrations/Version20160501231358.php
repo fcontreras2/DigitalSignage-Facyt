358f77e92985e3bc9a39d981ce4804ee2c71ca31
fcontreras2 <freddy.contreras3@gmail.com>
2016-05-01 23:19:57 -0430

    + status nuevos de los canales

--- app/DoctrineMigrations/Version20160501231358.php
+++ app/DoctrineMigrations/Version20160501231358.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160501230119 extends AbstractMigration
+class Version20160501231358 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,8 @@ class Version20160501230119 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE channel ALTER status TYPE INT');
+        $this->addSql('ALTER TABLE channel ALTER status TYPE INT USING status::integer');
+        $this->addSql('ALTER TABLE channel ALTER status SET DEFAULT 1');
     }
 
     /**
@@ -30,5 +31,6 @@ class Version20160501230119 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE channel ALTER status TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE channel ALTER status DROP DEFAULT');
     }
 }
